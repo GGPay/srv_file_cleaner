3c075cb526576e796b614660dc4b75b6c68db5a5
RomanP <usggpay@gmail.com>
2022-06-20 18:23:16 -0500

    clean up model

--- app/models/orm/db_schema.py
+++ app/models/orm/db_schema.py
@@ -1,6 +1,5 @@
 from .base import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class Product(Base):
@@ -8,28 +7,15 @@ class Product(Base):
     productID = Column(Integer, primary_key=True, nullable=False)
     partNumber = Column(String(255))
     factoryID = Column(Integer)
-    # relationship
-    pr_product = relationship("OrderDetails", backref="product")
-
 
 
 class Order(Base):
     __tablename__ = "order"
     orderID = Column(Integer, primary_key=True, nullable=False)
-    # relationship
-    #or_product = relationship("OrderDetails", backref="odr_product")
-
-    __mapper_args__ = {"eager_defaults": True}
 
 
 class OrderDetails(Base):
     __tablename__ = "orderDetails"
     orderDetailID = Column(Integer, primary_key=True, nullable=False)
     productID = Column(Integer, ForeignKey("product.productID"), nullable=False)
-    #orderID = Column(Integer, ForeignKey("order.orderID"), nullable=False)
     description = Column(String(255))
-    # relationship
-    #odr_product = relationship("Product", backref="pr_product", uselist=False)
-    #odr_order = relationship("Order", backref="or_product", uselist=False)
-
-
--- app/models/pydantic/product.py
+++ app/models/pydantic/product.py
@@ -9,12 +9,3 @@ class ProductModel(BaseModel):
     class Config:
         orm_mode = True
         allow_population_by_field_name = True
-
-# class ProductModel(BaseModel):
-#     productID: int = Field(..., alias='product_id')
-#     factoryID: int = Field(..., alias='factory_id')
-#     partNumber: str = Field(..., alias='part_number')
-#
-#     class Config:
-#         orm_mode = True
-#         allow_population_by_field_name = True
