b900259ef4f801a86625edacb76221f12d4c8390
RomanP <usggpay@gmail.com>
2022-06-21 08:56:17 -0500

    pydantic response

--- app/models/pydantic/product.py
+++ app/models/pydantic/product.py
@@ -1,10 +1,23 @@
 from pydantic import BaseModel, Field, NoneStr
+from typing import Dict
 
 
 class ProductModel(BaseModel):
     description: NoneStr = Field(..., alias='description')
     factoryID: int = Field(..., alias='factory_id')
     partNumber: str = Field(..., alias='part_number')
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+
+
+class RepositoryDetail(BaseModel):
+    factoryID: int
+    target_partNumber: str = Field(..., alias='target_part_number')
+    partNumber: str = Field(..., alias='part_number')
+    match_score: float
+    description: str = Field(..., alias='description')
 
     class Config:
         orm_mode = True
--- app/routes/product.py
+++ app/routes/product.py
@@ -1,17 +1,20 @@
 from fastapi import APIRouter, Depends, HTTPException
+from typing import List
 import pandas as pd
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 from app.dependencies.db import get_db
 from app.models.orm.queries import product as product_utils
 from app.backend.utils import StringClean
+from app.models.pydantic.product import RepositoryDetail
 import app.backend.feature_extractor as fe
 
 router = APIRouter()
 
 
-@router.get("/product", status_code=status.HTTP_200_OK, tags=["Products"])
+@router.get("/product", status_code=status.HTTP_200_OK, tags=["Products"], response_model=List[RepositoryDetail])
 async def get_product_matching_score(part_number: str, factory_id: int, db: AsyncSession = Depends(get_db)):
+
     products = await product_utils.get_products_by_factory_id(factory_id, db)
     clean_part_num = StringClean.clean_string(part_number)
 
@@ -25,13 +28,21 @@ async def get_product_matching_score(part_number: str, factory_id: int, db: Asyn
     df['partNumber'] = df['partNumber'].str.lstrip('0').fillna(value='0')
 
     # calculate levenstein distance and score
-    df['levenshteinDistance'] = df.apply(fe.sorted_levenshtein_apply, axis=1)
-    df['uniqueNumberCount'] = df.apply(fe.get_unique_number_count, axis=1) + 1
-    df['numberMatchRate'] = df.apply(fe.get_rate, axis=1)
-    df['matchScore'] = df.apply(fe.sorted_levenshtein_rate_apply, axis=1)
-    cols = ['factoryID', 'partNumber', 'target_partNumber', 'matchScore', 'description']
-    df = df.nlargest(3, 'matchScore')
+    df['levenshtein_distance'] = df.apply(fe.sorted_levenshtein_apply, axis=1)
+    df['unique_number_count'] = df.apply(fe.get_unique_number_count, axis=1) + 1
+    df['number_match_rate'] = df.apply(fe.get_rate, axis=1)
+    df['match_score'] = df.apply(fe.sorted_levenshtein_rate_apply, axis=1)
+    cols = ['factoryID', 'partNumber', 'target_partNumber', 'match_score', 'description']
+    df = df.nlargest(3, 'match_score')
     df_result = df[cols]
 
+    product_score = df_result.to_dict('records')
 
-    return products
+    context = []
+    i = 0
+    for _ in product_score:
+        context.append(RepositoryDetail.parse_obj(product_score[i]).dict())
+        i += 1
+
+    return context
+
